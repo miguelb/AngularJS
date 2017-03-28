2cc1e072cc44de1c5742da972967ee7dc30d238a
Srinivas Iyer <srinivas.iyer@hotmail.com>
2017-03-28 15:05:19 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', '$filter','$timeout', function($scope, $filter, $timeout) {
+    
+    $scope.handle = '';
+    $scope.lowercasehandle = function() {
+        return $filter('lowercase')($scope.handle);
+    };
+    
+    $scope.characters = 5;
+    
+    $scope.rules = [
+        { rulename: "Must be 5 characters"},
+        { rulename: "Must not be used elsewhere"},
+        { rulename: "Must be cool"}
+    ];
+    console.log($scope.rules);
+}]);
